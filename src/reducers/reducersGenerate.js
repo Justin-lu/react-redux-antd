743865025d4f77010d318d359308ad788919a71d
Justin <gdjyluxiaoyong@gmail.com>
2016-12-07 23:30:21 +0800

    refactor reducersGenerate function

--- src/reducers/reducersGenerate.js
+++ src/reducers/reducersGenerate.js
@@ -1,31 +1,47 @@
-export default function reducersGenerate(actionType, initialState){
+import _ from 'lodash';
+
+export default function reducersGenerate(actionType, initialState, optionsHandler){
+  if(!_.isArray(actionType)){
+    actionType = [actionType];
+  }
+
+  const defaultHandler = {};
+
+  _.each(actionType, (item) => {
+    // PENDING action
+    defaultHandler[`${item}_PENDING`] = (state) => {
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    };
+
+    // FULFILLED action
+    defaultHandler[`${item}_FULFILLED`] = (state, action) => {
+      return {
+        isFetching: false,
+        isFulfilled: true,
+        data: action.payload.data,
+        meta:   {
+          total: action.payload.meta.total,
+          perPage: action.payload.meta.per_page,
+          page: action.payload.meta.page
+        }
+      };
+    };
+
+    // REJECTED action
+    defaultHandler[`${item}_REJECTED`] = (state, action) => {
+      return Object.assign({}, state, {
+        isFetching: false,
+        isRejected: true,
+        error: action.payload
+      });
+    };
+  });
+
+  const actionHanlder = Object.assign({}, defaultHandler, optionsHandler);
+
   return (state = initialState, action) => {
-    switch (action.type) {
-      case `${actionType}_PENDING`:
-        return Object.assign({}, state, {
-          isFetching: true
-        });
-
-      case `${actionType}_FULFILLED`:
-        return {
-          isFetching: false,
-          isFulfilled: true,
-          data: action.payload.data,
-          meta: {
-            total: action.payload.meta.total,
-            perPage: action.payload.meta.per_page,
-            page: action.payload.meta.page
-          }
-        };
-
-      case `${actionType}_REJECTED`:
-        return Object.assign({}, state, {
-          isFetching: false,
-          isRejected: true,
-          error: action.payload
-        });
-
-      default: return state;
-    }
+    return actionHanlder[action.type] && actionHanlder[action.type](state, action) || state;
   };
 }
